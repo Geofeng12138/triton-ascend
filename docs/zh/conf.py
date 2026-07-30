454740d7eaadce0d660ce9fd6c6264653785fca8
yanggaofeng (C) <y60124509@china.huawei.com>
2026-07-30 11:45:33 +0800

    update translate

--- docs/zh/conf.py
+++ docs/zh/conf.py
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 import os
+import sys
 # General information about the project.
 
 project = 'Triton Ascend'
